6f0056aa7db926e82591c35e3bbfc77e0b441926
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-02-05 17:42:02 -0500

    Routes

---
+++ src/routes/routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const { getDatabase } = require("../database/connection")
+
+const routes = express.Router();
+
+routes.route("/api/products").get(async (request, response) => {
+    const collection = getDatabase().collection("products");
+    
+    const queryPage = parseInt(request.query.page + ""); // Sanitize?
+    const page = queryPage > 0 ? queryPage : 1; // Maybe additional checks?
+    
+    const filter = request.query.filter + ""; // TODO: Sanitize? Important!!!
+    const options = filter !== "undefined" ? { name: { $regex: filter, $options: "i" }} : {}; // Ignore case
+    
+    collection.find(options, { projection: { _id: 0 } }).skip((page - 1) * 8).limit(8).toArray((error, result) => {
+        if(error) {
+            response.status(502).send("Error fetching products.");
+        } else {
+            // [] or [products]
+            response.json(result);
+        }
+    })
+});
+
+routes.route("/api/product/:productURL").get(async (request, response) => {
+    const collection = getDatabase().collection("products");
+    const productURL = request.params.productURL;
+    
+    collection.findOne({ url: productURL }, { projection: { _id: 0 } }, (error, result) => {
+        if(error) {
+            response.status(502).send("Error fetching product " + productURL + ".");
+        } else {
+            // null or product
+            response.json(result);
+        }
+    })
+})
+
+module.exports = routes;
