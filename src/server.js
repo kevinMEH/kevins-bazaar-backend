cd72fb0f4b661cdda229356054c87dcc48df3bdf
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-02-05 17:42:06 -0500

    Server

---
+++ src/server.js
@@ -0,0 +1,30 @@
+// The path is relative to the path node is started in.
+// We are running node from project root.
+require("dotenv").config({ path: "./config.env"});
+
+const express = require("express");
+const cors = require("cors");
+const database = require("./database/connection");
+
+const PORT = process.env.PORT || 80;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/routes"))
+
+app.use((err, _req, res) => {
+    console.error(err.stack);
+    res.status(500).send("Server Unavailable")
+});
+
+database.connectToServer((error = false) => {
+    if(error) {
+        console.error(error);
+        process.exit();
+    }
+    
+    app.listen(PORT, () => {
+        console.log("Server is running on port: " + PORT);
+    });
+});
